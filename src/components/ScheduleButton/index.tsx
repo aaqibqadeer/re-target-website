cbb42aa420e4c7b606d7eee89e5392d66ab59057
aaqibqadeer <aaqibqs@gmail.com>
2025-01-16 16:04:14 -0500

    fix: button font size

--- src/components/ScheduleButton/index.tsx
+++ src/components/ScheduleButton/index.tsx
@@ -5,7 +5,7 @@ export const ScheduleButton = () => {
         href='https://cta-redirect.hubspot.com/cta/redirect/6247171/7488f30e-10d9-4460-a794-824a61efb4ac'
         target='_blank'
         rel='noopener noreferrer'
-        className='inline-flex items-center px-12 py-6 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-150 ease-in-out'
+        className='text-2xl inline-flex items-center px-8 py-6 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-150 ease-in-out'
       >
         Schedule a meeting
       </a>
