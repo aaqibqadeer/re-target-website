4b0caca0c28d64057a0ee3add986d8cd780ad42e
aaqibqadeer <aaqibqs@gmail.com>
2025-01-22 02:32:12 -0500

    SEO content

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>RE-Target Agent Direct: Available Markets</title>
+        <meta
+          name='description'
+          content='Advertise to REALTORS, Advertise to Real Estate Agents, Market to REALTOS, Market to real estate agents'
+        />
+        <meta
+          name='keywords'
+          content='Advertise, REALTORS, Advertise to Real Estate Agents, Market to REALTOS, Market to real estate agents'
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
---
+++ test.js
@@ -0,0 +1,2 @@
+a;
+console.log(a) // 10
